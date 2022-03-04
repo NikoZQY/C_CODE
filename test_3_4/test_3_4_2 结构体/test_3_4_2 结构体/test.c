86f94ef6b84be40fe387859a519f67fae5bdae2e
NikoZQY <1179085770@qq.com>
2022-03-04 16:36:04 +0800

    3.4

---
+++ test_3_4/test_3_4_2 结构体/test_3_4_2 结构体/test.c
@@ -0,0 +1,17 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+struct Stu
+{
+	char name[20];
+	int age;
+	double score;
+};
+int main()
+{
+	struct Stu s = { "ÕÅÈý",20,99.9 };
+	printf("1:%s %d %lf\n", s.name, s.age, s.score);
+	struct Stu * ps = &s;
+	printf("2:%s %d %lf\n",(*ps).name, (*ps).age, (*ps).score);
+	printf("3:%s %d %lf\n",ps->name, ps->age, ps->score);
+	return 0;
+}
