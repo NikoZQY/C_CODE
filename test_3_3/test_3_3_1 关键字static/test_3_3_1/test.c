cd193d5155846da625a5ddec28ab5a59bb0b4d03
NikoZQY <1179085770@qq.com>
2022-03-04 16:35:29 +0800

    3.3

---
+++ test_3_3/test_3_3_1 关键字static/test_3_3_1/test.c
@@ -0,0 +1,18 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+void test()
+{
+	static int a = 1;
+	a++;
+	printf("%d", a);
+}
+int main()
+{
+	int i = 0;
+	while (i < 10)
+	{
+		test();
+		i++;
+	}
+	return 0;
+}
