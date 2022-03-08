2ebb0e6d6d66c2187f5828f870eedb4f2758e16c
NikoZQY <1179085770@qq.com>
2022-03-08 18:18:19 +0800

    3.8

---
+++ test_3_8/test_3_8_2 N的阶乘/test_3_8_2 N的阶乘/test.c
@@ -0,0 +1,15 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+int main()
+{
+	int n=0;
+	int i = 0;
+	int sum=1;
+	scanf("%d", &n);
+	for (i = 1; i <= n; i++)
+	{
+		sum *= i;
+	}
+	printf("%d\n",sum);
+	return 0;
+}
